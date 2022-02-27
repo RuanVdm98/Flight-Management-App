fc09402cc61154bf684686f17c2a10571e6b3304
Ruan VDM <rvandermerwe@jhb.dvt.co.za>
2022-02-27 22:16:50 +0200

    Format: reformatted files and changed implementation

--- src/map.ts
+++ src/map.ts
@@ -1,8 +1,93 @@
 import L from "leaflet";
+import {Flight} from "./models/flight";
 
 export const map = L.map("map-container").setView([20.505, -0.09], 2);
+let markers: L.Marker[] = [];
+const flights: Flight[] = [];
 
+const toDateTime = (seconds: number) => new Date(seconds * 1000);
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
+
+let tempFlights = window.localStorage.getItem('flights');
+
+if (tempFlights && tempFlights.length > 0) {
+    tempFlights = JSON.parse(tempFlights);
+    for (let i = 0; i < 30; i++) {
+        if (tempFlights[i][5] !== null || tempFlights[i][6] !== null) {
+            const flight: Flight = {
+                icoa24: tempFlights[i][0],
+                longitude: parseInt(tempFlights[i][5]),
+                latitude: parseInt(tempFlights[i][6]),
+                date: parseInt(tempFlights[i][3]),
+                altitude: parseInt(tempFlights[i][13]),
+                country: tempFlights[i][2],
+                velocity: parseInt(tempFlights[i][9]),
+                callSign: tempFlights[i][1]
+            };
+            flights.push(flight);
+        }
+    }
+    addFlightinfo();
+}
+
+export function addFlightinfo() {
+    flights.forEach((f) => {
+        const html = `  
+    <div class="card">
+      <div class="heading">
+        <h1 class="font-semibold">${f.callSign}</h1>
+        <label>${f.country}</label>
+      </div> 
+      <label>Updated: ${toDateTime(f.date)}</label>
+      <label>Velocity: ${f.velocity}</label>
+      <label>Altitude: ${f.altitude}m</label>
+    </div>`;
+        const infocontainer = document.getElementById("info-container");
+        if (infocontainer) {
+            infocontainer.innerHTML += html;
+        }
+    });
+
+    addMarkersToMap();
+}
+
+export function addMarkersToMap() {
+    flights.forEach((f) => {
+        markers.push(L.marker([f.latitude, f.longitude], {title: f.callSign}).addTo(map));
+    });
+}
+
+export function updateValue(e: Event) {
+    const infoContainer = document.getElementById("info-container");
+    if (infoContainer) {
+        infoContainer.innerHTML = "";
+    }
+    markers.forEach((m) => {
+        map.removeLayer(m);
+    });
+    markers = [];
+    flights
+        .filter((f) => f.callSign.toLowerCase().includes((e.target as HTMLInputElement).value.toLowerCase()))
+        .forEach((f) => {
+            const html = `  
+      <div class="card">
+        <div class="heading">
+          <h1 class="font-semibold">${f.callSign}</h1>
+          <label>${f.country}</label>
+        </div> 
+        <label>Updated: ${toDateTime(f.date)}</label>
+        <label>Velocity: ${f.velocity}</label>
+        <label>Altitude: ${f.altitude}m</label>
+      </div>`;
+            const infoContainer = document.getElementById("info-container");
+            if (infoContainer) {
+                infoContainer.innerHTML += html;
+            }
+            markers.push(L.marker([f.latitude, f.longitude], {title: f.callSign}).addTo(map));
+        });
+}
+
+
--- src/service.ts
+++ src/service.ts
@@ -1,3 +1,21 @@
 import {fromFetch} from "rxjs/fetch";
+import {catchError, map, of, switchMap, take, tap} from "rxjs";
 
-const flightData$ = fromFetch("https://opensky-network.org/api/states/all");
+const flightData$ = fromFetch("https://opensky-network.org/api/states/all").pipe(
+    take(30),
+    switchMap(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            return of({ error: true, message: `Error ${response.status}` });
+        }
+    }),
+    catchError(err => {
+    // Network or other error, handle appropriately
+    console.error(err);
+    return of({ error: true, message: err.message })
+    })
+);
+
+//tried to tap on the pipe and could not gte the data set the only way i could was by subscribing to the observable and then setting the local storage
+flightData$.subscribe(f => window.localStorage.setItem('flights', JSON.stringify(f.states)));
